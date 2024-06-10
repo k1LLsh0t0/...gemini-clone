a54b1dd021f05badb5d634e101fd5cc67c152512
k1LLsh0t0 <avinash.singh9214@gmail.com>
2024-06-10 19:27:27 +0530

    Basic structure complete including :Navbar,searchbox,sidebar,etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Sidebar from './components/Sidebar/Main/Sidebar/Sidebar'
-import Main from './components/Sidebar/Main/Main/Main'
-
+import Sidebar from './components/Sidebar/Main/Sidebar/Sidebar.jsx'
+import Main from './components/Sidebar/Main/Main/Main.jsx'
 const App = () => {
   return (
     <>
--- src/components/Sidebar/Main/Main/Main.jsx
+++ src/components/Sidebar/Main/Main/Main.jsx
@@ -1,9 +1,52 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './Main.css'
+import { assets } from '../../../../assets/assets'
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p><span>Hello,Avinash</span></p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Sbriefly summarize this concept:Urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activites for our work retreat </p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+        <div className="main-bottom">
+          <div className="searchbox">
+            <input type="text" placeholder="Enter a prompt here" />
+            <div>
+              <img src={assets.gallery_icon} alt=""/>
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info,including about people,so double-check its responses. Your privacy and Gemini Apps
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
